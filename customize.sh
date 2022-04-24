d025a3658bf363b6a307285c855751d826d041e2
Takeda-senpai <84650617+HuskyDG@users.noreply.github.com>
2022-04-24 23:53:13 +0700

    add busybox

--- customize.sh
+++ customize.sh
@@ -1,2 +1,7 @@
 chmod 755 "$MODPATH/system/bin/magic-flash"
+chmod 755 -R "$MODPATH/busybox"
 ln -sf ./magic-flash "$MODPATH/system/bin/flash"
+api_level_arch_detect
+ui_print "- Installing busybox for flashtool..."
+cp "$MODPATH/busybox/busybox-$ARCH" "$MODPATH/busybox/busybox"
+rm -rf "$MODPATH/busybox/busybox-"*
